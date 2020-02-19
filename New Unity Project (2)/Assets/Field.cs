1bec70e80b5e414154b0d042aa7fc32c32ec6b36
madssiggy <kanininaritainndahala@gmail.com>
2020-02-19 16:21:21 +0900

    ステージが回転するようになった

---
+++ New Unity Project (2)/Assets/Field.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+   public GameObject Manager;
+   Manager script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Manager = GameObject.Find("Manager");
+        script = Manager.GetComponent<Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Rotation = script.Rotation;
+
+        this.transform.Rotate(new Vector3 (0f,0f,Rotation)* Time.deltaTime);
+
+    }
+}
---
+++ New Unity Project (2)/Assets/Manager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Manager : MonoBehaviour
+{
+    public float Rotation=0f;
+    public const float AddRotation = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rotation = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            Rotation -= AddRotation;
+        }
+        if (Input.GetKeyDown(KeyCode.RightShift)) {
+            Rotation += AddRotation;
+        }
+
+    }
+//    public float GetRotation()
+//    {
+//        float tmp = Mathf.Abs(Rotation) / 1f;//絶対値を用意して、小数点以下を切る。
+
+//        if (tmp > 360f)
+//            tmp %= 360;
+
+//        Debug.Log("Rotate:" + tmp);
+//        return tmp/90;//角度を0,1,2,3で返す
+
+
+//    }
+//public bool isRotate()
+//    {
+//        float tmp = Mathf.Abs(Rotation) / 1f;
+//        if (tmp > 360f)
+//            tmp %= 360;
+//        switch (tmp) {
+//            case 0:
+//                return true;                ;
+//                break;
+//            case 1f:
+//            default:
+//                break;
+
+//        }
+
+//        return false;
+//    }
+}
